a4d289d96d3f9da96b3dea0e6e5f16daa260e076
Sean Grady <scgrady@smu.edu>
2024-02-07 10:19:07 -0600

    Updated Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./Pages/SignIn.tsx";
-import SignUp from "./Pages/SignUp.tsx";
+import SignUpProfessional from "./Pages/SignUpProfessional.tsx";
 import Home from "./Pages/Home.tsx";
 import Navigation from "./Components/Navigation/Navigation.tsx";
 import AccountTypeSelector from "./Pages/AccountTypeSelector.tsx";
@@ -16,8 +16,8 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path="signin" element={<SignIn/>}/>
         <Route path="signup" element={<AccountTypeSelector/>}/>
-        <Route path="signup/professional" element={<SignUp/>}/>
-        <Route path="signup/employer" element={<SignUp/>}/>
+        <Route path="signup/professional" element={<SignUpProfessional/>}/>
+        {/* <Route path="signup/employer" element={<SignUp/>}/> */}
       </Route>
   </Routes>
   );
