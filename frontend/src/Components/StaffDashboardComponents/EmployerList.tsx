efc48b0d707ae4a76a1e54dc7f52a5644a22c4ec
Son Phan <phantrungsonjoe@gmail.com>
2024-02-12 14:49:47 -0600

    fix up edit account for staff

--- frontend/src/Components/StaffDashboardComponents/EmployerList.tsx
+++ frontend/src/Components/StaffDashboardComponents/EmployerList.tsx
@@ -225,11 +225,7 @@ const EmployerList = () => {
               >
                 Save
               </button>
-              <button
-                className="bg-blue-600 text-white px-4 py-2 rounded-lg"
-              >
-                Delete
-              </button>
+              
             </div>
             
     </div>
--- frontend/src/Components/StaffDashboardComponents/ProfessionalList.tsx
+++ frontend/src/Components/StaffDashboardComponents/ProfessionalList.tsx
@@ -285,11 +285,7 @@ const ProfessionalList = () => {
               >
                 Save
               </button>
-              <button
-                className="bg-blue-600 text-white px-4 py-2 rounded-lg"
-              >
-                Delete
-              </button>
+              
             </div>
             
     </div>
